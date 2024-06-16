ad81f3e5a31dc4ebcc27e8bf1861e4896b9662b3
Eamon Burns (Laptop WSL) <me@eamonburns.com>
2024-06-16 00:20:34 -0700

    feat: rework activate script

--- bin/activate.sh
+++ bin/activate.sh
@@ -1,9 +1,10 @@
 # run using `source <scriptname>`
 
-alias b="${PWD:-.}/bin/build.sh"
+dir=${PWD:-.}
+
+alias b="${dir}/bin/build.sh"
 unalias r > /dev/null 2>&1
 r () {
-    basename=${1:-asem}
-    b "$basename"
-    ./build/"${basename}" # Run compiled executable
+    # Build and run compiled executable
+    b "$1" && ./build/"$(basename -s .asm "$1")"
 }
